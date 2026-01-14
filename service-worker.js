19c3ec8ead9721bb9fbfa44712b22a3a42678a94
danlegnew-bit <dan.legnew@gmail.com>
2026-01-14 20:27:08 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,20 @@
-const CACHE_NAME = 'fitness-calendar-v1';
+const CACHE_NAME = 'fitness-calendar-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Установка Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Активация и очистка старых кэшей
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Перехват запросов
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Возвращаем из кэша или делаем сетевой запрос
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
